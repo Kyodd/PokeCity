9da0b51bbe093c5d29bb9d304f2b21af7da56fea
stephaned7 <delaporte.stephane7@gmail.com>
2023-07-28 14:42:50 +0200

    map page

---
+++ js/map.js
@@ -0,0 +1,6 @@
+const btn =document.getElementsByClassName("btn");
+const view = document.getElementsByClassName("view");
+
+btn.addEventListener("click",function(event){
+    image.classList.toggle("show");
+})
